b0e5131f0b83a22fc4964cbb08fa61872dffdd25
Rodrigo Galante <rodrigo.galante@globo.com>
2020-03-28 23:11:47 -0300

    foi criado teste Calculadora_RetornarValoresSomaCorretos

--- TestesDeSoftware/1.1 - Testes Basicos/Demo.Tests/CalculadoraTests.cs
+++ TestesDeSoftware/1.1 - Testes Basicos/Demo.Tests/CalculadoraTests.cs
@@ -16,5 +16,25 @@ namespace Demo.Tests
             // Assert
             Assert.Equal(4, resultado);
         }
+
+        [Theory]
+        [InlineData(1, 1, 2)]
+        [InlineData(2, 1, 3)]
+        [InlineData(2, 2, 4)]
+        [InlineData(2, 3, 5)]
+        [InlineData(5, 3, 8)]
+        [InlineData(9, 9, 18)]
+        [InlineData(99, 100, 199)]
+        public void Calculadora_Somar_RetornarValoresSomaCorretos(double v1, double v2, double total)
+        {
+            // Arrange
+            var calculadora = new Calculadora();
+
+            // Act
+            var resultado = calculadora.Somar(v1, v2);
+
+            // Assert
+            Assert.Equal(total, resultado);
+        }
     }
 }
